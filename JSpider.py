f682ca7ee15c94c308169c2b67c53f611dd9c981
gsdmlgla <gsdmlgla@naver.com>
2015-07-06 15:51:22 -0700

    yolo

--- JSpider.py
+++ JSpider.py
@@ -240,8 +240,8 @@ class JSpiderLeg:
 		
 		joint0AngleInRadius = math.atan2(root2EEDirection[1], root2EEDirection[0])
 		
-		yVectorOnPlane = Vector3.project(root2EEDirection[1], root2EEDirection[0])
-		xVectorOnPlane = root2EEDirection - yVectorOnPlane
+		yVectorOnPlane = Vector3.project(root2EEDirection, [0, 0, 1])
+		xVectorOnPlane = Vector3.subtract(root2EEDirection, yVectorOnPlane)
 		y = (1 if yVectorOnPlane.z > 0 else -1) * Vector3.length(yVectorOnPlane)
 		planarAngleFromRootToEEInRadius = math.atan2(y, Vector3.length(xVectorOnPlane))
 		
