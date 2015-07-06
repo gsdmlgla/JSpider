270f5ff81615c0eadd4476f47f3c7c6fbbfd0a9d
gsdmlgla <gsdmlgla@naver.com>
2015-07-06 16:30:10 -0700

    yolo

--- JSpider.py
+++ JSpider.py
@@ -252,8 +252,8 @@ class JSpiderLeg:
 		D = Vector3.length(root2EEDirection)
 		cosVal = -(math.pow(a2, 2) - math.pow(a1, 2) - math.pow(D, 2)) / (2 * a1 * D)
 		joint1AngleInRadius = math.acos(min(1, max(cosVal, -1)) + planarAngleFromRootToEEInRadius
-		cosVal = -(math.pow(D, 2) - math.pow(a1, 2) - math.pow(a2, 2))
-		angle2 = math.acos(min(1, max(cosVal, -1)) / (2 * a1 * a2))
+		cosVal = -(math.pow(D, 2) - math.pow(a1, 2) - math.pow(a2, 2)) / (2 * a1 * a2)
+		angle2 = math.acos(min(1, max(cosVal, -1)))
 		joint2AngleInRadius = angle2 - 3.14159265358979
 		
 		joint0Angle = math.degrees(joint0AngleInRadius)
